6e5c1b4a0b89b564883a726951c376bcb990e112
sjparkk <sjparkk0820@naver.com>
2021-11-24 23:32:12 +0900

    git init

---
+++ src/main/java/com/growup/streamquiz/StreamQuizApplication.java
@@ -0,0 +1,13 @@
+package com.growup.streamquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/growup/streamquiz/StreamQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growup.streamquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
