5c1e598b73d8fdeb09382cf258bc8ac5aafae69e
sjparkk <sjparkk0820@naver.com>
2021-11-25 22:26:16 +0900

    feat : Quiz4에 문제를 위한 클래스 및 초기값 세팅

--- src/main/java/com/growup/streamquiz/quiz/Quiz4.java
+++ src/main/java/com/growup/streamquiz/quiz/Quiz4.java
@@ -1,4 +1,51 @@
 package com.growup.streamquiz.quiz;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Quiz4 {
+
+    private static List<Transaction> transactions;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Trader {
+        private String name;
+        private String city;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Transaction {
+        private Trader trader;
+        private int year;
+        private int value;
+    }
+
+    //초기값
+    public static void init() {
+        Trader kyu = new Trader("Kyu", "Seoul");
+        Trader ming = new Trader("Ming", "Gyeonggi");
+        Trader hyuk = new Trader("Hyuk", "Incheon");
+        Trader hwan = new Trader("Hwan", "Seoul");
+
+        transactions = Arrays.asList(
+                new Transaction(kyu, 2019, 30000),
+                new Transaction(kyu, 2020, 12000),
+                new Transaction(ming, 2020, 40000),
+                new Transaction(ming, 2020, 7100),
+                new Transaction(hyuk, 2019, 5900),
+                new Transaction(hwan, 2020, 4900));
+    }
+
+
 }
