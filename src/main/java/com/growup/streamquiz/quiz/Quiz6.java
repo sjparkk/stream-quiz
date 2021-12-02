bc60bf595bdfd740b28d12e4e1853feb3c4eb01c
sjparkk <sjparkk0820@naver.com>
2021-12-02 23:01:47 +0900

    feat : Quiz6 문제를 위한 클래스와 기본 데이터값 추가

---
+++ src/main/java/com/growup/streamquiz/quiz/Quiz6.java
@@ -0,0 +1,52 @@
+package com.growup.streamquiz.quiz;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Quiz6 {
+
+    private static Student[] studentArr;
+
+    @Getter
+    @AllArgsConstructor
+    @Setter
+    public static class Student {
+        private String name;
+        private boolean isMale; // 성별
+        private int hak; // 학년
+        private int ban; // 반
+        private int score;
+
+        public String toString() {
+            return String.format("[%s, %s, %d학년 %d반, %3d점 ]", name, isMale ? "남" : "여", hak, ban, score);
+        }
+
+        // groupingBy()에서 사용 성적을 상,중,하 세 단계로 분류
+        enum Level {
+            HIGH, MID, LOW
+        }
+    }
+
+    public static void initData() {
+        studentArr =  new Student[]{
+                new Student("나자바", true, 1, 1, 300),
+                new Student("김지미", false, 1, 1, 250),
+                new Student("김자바", true, 1, 1, 200),
+                new Student("이지미", false, 1, 2, 150),
+                new Student("남자바", true, 1, 2, 100),
+                new Student("안지미", false, 1, 2, 50),
+                new Student("황지미", false, 1, 3, 100),
+                new Student("강지미", false, 1, 3, 150),
+                new Student("이자바", true, 1, 3, 200),
+                new Student("나자바", true, 2, 1, 300),
+                new Student("김지미", false, 2, 1, 250),
+                new Student("김자바", true, 2, 1, 200),
+                new Student("이지미", false, 2, 2, 150),
+                new Student("남자바", true, 2, 2, 100),
+                new Student("안지미", false, 2, 2, 50),
+                new Student("황지미", false, 2, 3, 100),
+                new Student("강지미", false, 2, 3, 150),
+                new Student("이자바", true, 2, 3, 200) };
+    }
+}
